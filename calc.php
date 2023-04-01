0f3caba74f4730f1630a19b2e101ac267af1bd30
MinYuqShu <121721144+MinYuqShu@users.noreply.github.com>
2023-04-01 23:59:29 +0900

    Add files via upload

---
+++ calc.php
@@ -0,0 +1,14 @@
+<?php
+$num1=$_POST['number1'];
+$num2=$_POST['number2'];
+$num3 = $num1+$num2;
+$num4 = $num1-$num2;
+$num5 = $num1*$num2;
+$num6 = $num1/$num2;
+echo"$num1 + $num2 = $num3<br>";
+echo"$num1 - $num2 = $num4<br>";
+echo"$num1 * $num2 = $num5<br>";
+echo"$num1 / $num2 = $num6<br>";
+
+?>
+
